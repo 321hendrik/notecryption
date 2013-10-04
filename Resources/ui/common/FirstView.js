e0fc03de2767bf3f406101480f53a7fc432b8bfd
Hendrik Elsner <hendrik.elsner@hpm-kommunikation.de>
2013-10-04 16:18:46 +0200

    use ECB Blowfish encryption

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -28,14 +28,6 @@ function FirstView() {
 	});
 	container.add(labelHeadLine);
 	
-	// var labelKey = Ti.UI.createLabel({
-		// color:'#000',
-		// text: 'Enter Key',
-		// height: 60,
-		// width: 200
-	// });
-	// container.add(labelKey);
-	
 	var textAreaKey = Ti.UI.createTextField({
 		borderWidth: 2,
 		borderColor: '#bbb',
@@ -44,23 +36,13 @@ function FirstView() {
 		keyboardType: Ti.UI.KEYBOARD_DEFAULT,
 		returnKeyType: Ti.UI.RETURNKEY_DEFAULT,
 		textAlign: 'center',
-		value: 'Enter Key',
+		hintText: 'Enter Key',
+		value: '',
 		width: 200,
 		height: 30,
 		font: {fontSize: '16sp'}
 	});
-	textAreaKey.addEventListener('focus', function(e) {
-		textAreaKey.value = '';
-	});
 	container.add(textAreaKey);
-	
-	// var labelText = Ti.UI.createLabel({
-		// color:'#000',
-		// text: 'Enter Text',
-		// height: 60,
-		// width: 200
-	// });
-	// container.add(labelText );
 	
 	var spacer = Ti.UI.createView({
 		height: 20
@@ -75,85 +57,60 @@ function FirstView() {
 		keyboardType: Ti.UI.KEYBOARD_DEFAULT,
 		returnKeyType: Ti.UI.RETURNKEY_DEFAULT,
 		textAlign: 'center',
-		value: 'Enter Text',
+		hintText: 'Enter Text',
+		value: '',
 		width: 200,
 		height: 30,
 		font: {fontSize: '16sp'}
 	});
-	textAreaText.addEventListener('focus', function(e) {
-		textAreaText.value = '';
-	});
 	container.add(textAreaText);
 	
-	var btnCrypt = Ti.UI.createButton({
-		title: 'Crypt',
+	var buttonContainer = Ti.UI.createView({
 		top: 20,
-		center: {x: '50%', y: '50%'},
+		width: Ti.UI.SIZE,
+		height: Ti.UI.SIZE,
+		center: {x: '50%'},
+		layout: 'horizontal'
+	});
+	container.add(buttonContainer);
+	
+	var btnEncrypt = Ti.UI.createButton({
+		title: 'Encrypt',
 		width: 60,
 		height: 30,
 		font: {fontSize: '16sp'}
 	});
-	container.add(btnCrypt);
-	btnCrypt.addEventListener('singletap', function (e) {
-		alert('encrypted: '+encrypt(textAreaText.value, textAreaKey.value)+' decrypted : '+decrypt(encrypt(textAreaText.value, textAreaKey.value), textAreaKey.value));
+	buttonContainer.add(btnEncrypt);
+	btnEncrypt.addEventListener('singletap', function (e) {
+		var encrypted = encrypt(textAreaText.value, textAreaKey.value);
+		alert('encrypted: ' + encrypted);
+		textAreaText.value = encrypted;
+	});
+	var btnDecrypt = Ti.UI.createButton({
+		title: 'Decrypt',
+		width: 60,
+		height: 30,
+		font: {fontSize: '16sp'}
+	});
+	buttonContainer.add(btnDecrypt);
+	btnDecrypt.addEventListener('singletap', function (e) {
+		var decrypted = decrypt(textAreaText.value, textAreaKey.value);
+		alert('decrypted: ' + decrypted);
+		textAreaText.value = decrypted;
 	});
 	
-	var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-	var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	var germanSpecific = 'öäüÖÄÜß';
-	var numbers = '1234567890'
-	var varChars = ' !"§$%&/()=?*+#_-:.;,'
-	var alphaNumCharList = lowerCase + upperCase + germanSpecific + numbers + varChars;
-	var keyList = '';
-	
-	function reverse(text) {
-		var reversed = '';
-		for(var i=text.length,j=0;i>j;i--){
-			reversed += text[i];
-		}
-		return reversed;
-	}
-	
-	function popStr(text, index) {
-		var before = text.slice(0, index);
-		var after = text.slice( (index+1), text.length );
-		return before+after;
-	}
-	
+	// use ECB Blowfish plugin @TODO fix trailing zeros
+	var Blowfish = require("com.dmrsolutions.blowfish").Blowfish;
 	function encrypt(text, key) {
-		// encrypt function
-		var result;
-		keyList = '';
-		var outputText = '';
-		var cryptIndex = 0;
-		cryptIndexList = [];
-		keyList = key + alphaNumCharList;
-		// for(var i=0,j=text.length; i<j; i++){
-			// var revKeyList = reverse(keyList);
-			// revKeyList = popStr( revKeyList, revKeyList.indexOf(text[i]) );
-			// keyList = reverse(keyList);
-		// };
-		for(var i=0,j=text.length; i<j; i++){
-			result = keyList[alphaNumCharList.indexOf(text[i])];
-			outputText += result;
-		};
-		return outputText;
+		var bfEnc = new Blowfish(key);
+		var encryptedText = bfEnc.encrypt(text);
+		return encryptedText;
 	}
 	
 	function decrypt(text, key) {
-		// decrypt function
-		keyList = '';
-		var outputText = '';
-		keyList = key + alphaNumCharList;
-		// for(var i=0,j=text.length; i<j; i++){
-			// var revKeyList = reverse(keyList);
-			// revKeyList = popStr( revKeyList, revKeyList.indexOf(text[i]) );
-			// keyList = reverse(keyList);
-		// };
-		for(var i=0,j=text.length; i<j; i++){
-			outputText += alphaNumCharList[keyList.indexOf(text[i])];
-		};
-		return outputText;
+		var bfDec = new Blowfish(key);
+		var decryptedText = bfDec.decrypt(text);
+		return decryptedText;
 	}
 	
 	return self;
